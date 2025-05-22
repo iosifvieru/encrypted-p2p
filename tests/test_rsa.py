713f69aefaca63f56de7837e71e9cb9daf1ebf56
Iosif Vieru <iosifvieru440@gmail.com>
2025-05-22 13:36:54 +0300

    rsa tests

--- tests/test_rsa.py
+++ tests/test_rsa.py
@@ -8,7 +8,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import unittest
-from crypto.rsa import rsa_generate_keys, rsa_decrypt, rsa_encrypt
+from crypto.rsa import rsa_generate_keys, rsa_decrypt, rsa_encrypt, string_to_int, int_to_string
 
 class TestRSA(unittest.TestCase):
     def test_rsa(self):
@@ -26,6 +26,18 @@ class TestRSA(unittest.TestCase):
         cipher = rsa_encrypt(mesaj, public_key1)
 
         self.assertNotEqual(mesaj, rsa_decrypt(cipher, private_key2))
+
+    def test_string_to_int(self):
+        initial_string = "a"
+        int_value = string_to_int(initial_string)
+
+        self.assertEqual(int_value, 97)
+
+    def test_int_to_string(self):
+        int_value = 97
+        char_value = int_to_string(int_value)
+
+        self.assertEqual(char_value, 'a')
 
 if __name__ == "__main__":
     unittest.main()
